f1b287dabbb7f0fc0db1d961c9087abb489fecb5
kenjiding <872570730@qq.com>
2024-03-05 02:00:34 +1030

    feature deploy

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 import * as dotenv from 'dotenv';
+import 'reflect-metadata';
 
 const PORT = 3008;
 
