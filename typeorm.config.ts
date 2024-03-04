e5fcdba06f6bf7dc7c985769f3f6a630c28e283b
kenjiding <872570730@qq.com>
2024-03-05 01:54:18 +1030

    feature deploy

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -11,11 +11,11 @@ console.log(333, 'process.env.DB_HOST: ', process.env.DB_HOST);
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: '3.82.26.31',
+  port: 3090,
+  username: 'root',
+  password: '123456',
+  database: 'blog',
   timezone: 'UTC', // 设置时区为 UTC
   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
   synchronize: false, // 注意：生产环境中应该设置为 false
