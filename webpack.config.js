39b293edc19efcab482149e44f97dbff204f321b
kenjiding <872570730@qq.com>
2024-03-05 12:15:21 +1030

    feature deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 // fork-ts-checker-webpack-plugin需要单独安装
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: "./src/main",
-  target: "node",
+  entry: './src/main',
+  output: {
+    path: path.resolve(__dirname, 'nest-blog'), // 输出目录路径
+    filename: 'main.js'
+  },
+  target: 'node',
   // 置为空即可忽略webpack-node-externals插件
   externals: {},
   // ts文件的处理
@@ -15,32 +19,27 @@ module.exports = {
       {
         test: /\.ts?$/,
         use: {
-          loader: "ts-loader",
+          loader: 'ts-loader',
           options: { transpileOnly: true }
         },
         exclude: /node_modules/
       }
     ]
   },
-  // 打包后的文件名称以及位置
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  },
   resolve: {
-    extensions: [".js", ".ts", ".json"]
+    extensions: ['.js', '.ts', '.json']
   },
   plugins: [
     // 需要进行忽略的插件
     new webpack.IgnorePlugin({
       checkResource(resource) {
         const lazyImports = [
-          "@nestjs/microservices",
-          "@nestjs/microservices/microservices-module",
-          "@nestjs/websockets/socket-module",
-          "cache-manager",
-          "class-validator",
-          "class-transformer"
+          '@nestjs/microservices',
+          '@nestjs/microservices/microservices-module',
+          '@nestjs/websockets/socket-module',
+          'cache-manager',
+          'class-validator',
+          'class-transformer'
         ];
         if (!lazyImports.includes(resource)) {
           return false;
